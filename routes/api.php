eeeabf37908f1dc4495e62a13746bae8965769bc
letahaphuong <letahaphuong@gmail.com>
2023-09-11 14:59:44 +0700

    PB-BE-02 Create api login

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("v1/")->controller(AuthController::class)->group(function () {
+Route::prefix("v1/auth")->controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
