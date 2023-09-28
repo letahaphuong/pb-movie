0cab5846aabc772b976c5858ec5c9f8503d4cd40
letahaphuong <letahaphuong@gmail.com>
2023-09-28 11:36:28 +0700

    PB-BE-14 Create api search movie:

--- packages/movies/src/Repositories/MovieRepository.php
+++ packages/movies/src/Repositories/MovieRepository.php
@@ -7,4 +7,6 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 interface MovieRepository extends RepositoryInterface
 {
     public function fetchMovieWithMovieType($movieType);
+
+    public function searchMovie($keywords = '',$columns = [], $sortBys = [], $perPage = 0);
 }
--- packages/movies/src/Repositories/MovieRepositoryEloquent.php
+++ packages/movies/src/Repositories/MovieRepositoryEloquent.php
@@ -2,13 +2,14 @@
 
 namespace Package\Movie\Repositories;
 
+use App\Core\CoreModel\CoreModel;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Package\Movie\Models\Movie;
 use Prettus\Repository\Criteria\RequestCriteria;
 use Prettus\Repository\Eloquent\BaseRepository;
 
 class MovieRepositoryEloquent extends BaseRepository implements MovieRepository
 {
-
     public function model()
     {
         return Movie::class;
@@ -22,5 +23,20 @@ class MovieRepositoryEloquent extends BaseRepository implements MovieRepository
     public function fetchMovieWithMovieType($movieType)
     {
         return Movie::select('id', 'name')->where('movie_type_id', $movieType)->get();
+    }
+
+    public function searchMovie($keywords = '', $columns = [], $sortBys = [], $perPage = 0)
+    {
+        return Movie::select(
+            array_merge($columns, ['movies.id', 'movies.created_at'])
+        )
+            ->where(function (Builder $query) use ($columns, $keywords) {
+                scopeOrWhereLike($query, $columns, $keywords);
+            })
+            ->with(['medias' => function (Builder $query) {
+                $query->select('medias.id', 'medias.movie_id', 'medias.stored_key', 'medias.source_type');
+            }])
+            ->orderBy('movies.' . $sortBys['sort_by'], $sortBys['sort_type'])
+            ->paginate($perPage);
     }
 }
