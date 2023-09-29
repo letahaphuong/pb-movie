4adaa0ee20430332fe67619102c91a275a85b8c0
letahaphuong <letahaphuong@gmail.com>
2023-09-29 13:15:49 +0700

    PB-BE-16 Create api fetch movies by country

--- packages/movies/routes/api.php
+++ packages/movies/routes/api.php
@@ -22,6 +22,7 @@ Route::prefix("api/v1/movies")->controller(MovieController::class)->group(functi
     });
     Route::get('', 'fetchMovieForHomePage');
     Route::get('search', 'searchMovie');
+    Route::get('countries/{name}', 'fetchMoviesByCountry');
 });
 
 Route::prefix("api/v1/movie-episodes")->controller(MovieEpisodeController::class)->group(function () {
