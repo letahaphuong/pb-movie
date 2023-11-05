1b8f0aed8d1cf010f415e6f46a2af343ed348e2e
admin <admin@MBP-cua-Mac.mshome.net>
2023-11-05 22:12:29 +0700

    Fix web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('{any}', function () {
-    return view('React'); // or wherever your React app is bootstrapped.
-})->where('any', '.*');
+//Route::get('{any}', function () {
+//    return view('React'); // or wherever your React app is bootstrapped.
+//})->where('any', '.*');
